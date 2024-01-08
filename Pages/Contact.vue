5c4e08b4dc8d6bc03d226111d2a4d141070efab7
Puput Silva Rosiana <puputsilva73.com>
2024-01-08 15:32:17 +0700

    commit1

---
+++ Pages/Contact.vue
@@ -0,0 +1,35 @@
+<template>
+    <script src="https://cdn.tailwindcss.com"></script>
+
+<body class="bg-sky-400 flex">
+<div class="container mt-40 max-w-sm bg-white m-auto p-10 rounded-md shadow-lg">
+    <h1 class="text-3x1 font-thin text-center">
+        Form Contact
+    </h1>
+    <form action="" class="mt-5">
+        <label for="" class="block">
+            <span class="block font-semibold text-sm">
+                Nama Lengkap
+            </span>
+            <input type="text" class="w-full block mt-1 border border-sky-400 rounded-md">
+        </label>
+        <label for="" class="block mt-2">
+            <span class="block font-semibold text-sm">
+                No Handphone
+            </span>
+            <input type="text" class="w-full block mt-1 border border-sky-400 rounded-md">
+        </label>
+        <label for="" class="block mt-2">
+            <span class="block font-semibold text-sm">
+                Alamat
+            </span>
+            <input type="text" class="w-full block mt-1 border border-sky-400 rounded-md">
+        </label>
+    <button class="mt-4 text-sm bg-sky-600 border borde-1 px-2 py-1 rouded-md text-white">
+        Submit
+    </button>
+    </form>
+</div>
+</body>
+
+  </template>
---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+
+
+  <NuxtPage/>
+
+  <footer>
+
+  </footer>
+  </div>
+</template>
+
+<style>
+body {
+  font-family: sans-serif;
+
+}
+</style>
